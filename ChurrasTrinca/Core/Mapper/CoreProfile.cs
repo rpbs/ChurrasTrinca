97c2a96a8fdebd737b23ad95bdc2f693290e9fa4
renyo.borges <renyo.borges@hrmsolucoes.com.br>
2022-01-19 20:41:08 -0300

    Profile do autoMapper

--- ChurrasTrinca/Core/Mapper/CoreProfile.cs
+++ ChurrasTrinca/Core/Mapper/CoreProfile.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using AutoMapper;
+using Core.Entities;
+using Core.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,13 @@ using System.Threading.Tasks;
 
 namespace Core.Mapper
 {
-    public class CoreProfile
+    public class CoreProfile : Profile
     {
+        public CoreProfile()
+        {
+            // falta adicionar no startup
+            CreateMap<Participante, ParticipanteResponse>();
+            CreateMap<Churrasco, ChurrascoResponse>();
+        }
     }
 }
