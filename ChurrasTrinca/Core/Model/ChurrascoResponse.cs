4a1d0130aa50d4973c0dcda4a62ac60497739b17
renyo.borges <renyo.borges@hrmsolucoes.com.br>
2022-01-19 15:05:09 -0300

    Response do churrasco, onde será usado pra exibir os churrascos

---
+++ ChurrasTrinca/Core/Model/ChurrascoResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model
+{
+    public class ChurrascoResponse
+    {
+        public string Descricao { get; set; } 
+        public int QuantidadePessoas { get; set; }
+        public decimal ValorTotal { get; set; }
+
+    }
+}
