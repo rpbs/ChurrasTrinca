1515f77b186dd8ab9cafd2bedb08000c2eed6528
renyo.borges <renyo.borges@hrmsolucoes.com.br>
2022-01-19 20:40:58 -0300

    Classe base para consultar passando somente Id

---
+++ ChurrasTrinca/Core/Queries/BaseIdQuery.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Queries
+{
+    public class BaseIdQuery
+    {
+        public Guid Id { get; }
+
+        public BaseIdQuery(Guid id)
+        {
+            Id = id;
+        }
+    }
+}
